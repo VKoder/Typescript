85c54c9ecd2531f1dd01a9b36e370e0a9e21fb40
VKoder <vivekkhule204@gmail.com>
2024-05-22 07:38:36 +0530

    Type Annotations in Typescript

---
+++ Practice Questions/index.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+
+// 1) Declare a variable with a number type and assign it a value. Log the variable to the console.
+
+var num:number = 44
+console.log(num)
+
+
+
+// 2) Create a function that takes a string as an argument and returns its length. Use type annotations.
+
+function giveString(str:string):number{
+    return str.length
+}
+console.log(giveString("Hello"))
+
+
+// Declare an array of numbers and initialize it with some values. Log the array to the console.
+
+var arrayOfNum:number[]=[4,4,4,4,4]
+console.log(arrayOfNum)
